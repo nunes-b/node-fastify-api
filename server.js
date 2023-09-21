04984ad500440bd67fc6a674fa5e3f28d7bb38bc
Rômulo Nunes <romulo@geoflow.com.br>
2023-09-21 15:51:33 -0300

    chore: server

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-import { fastfy } from "fastify";
-import { dotenv } from "dotenv";
-dotenv.config();
+const { fastify } = require("fastify");
+require("dotenv").config();
 
 const portEnds = process.env.PORT;
-const server = fastfy();
+const server = fastify();
 
-server.listen({
-  port: portEnds,
+server.get("/", () => {
+  return "Hello World with fastify!";
+});
+
+server.listen(portEnds, (err) => {
+  if (err) {
+    console.error("Error starting the server:", err);
+    process.exit(1);
+  }
+  console.log(`Server is running on port http://localhost:${portEnds}/`);
 });
